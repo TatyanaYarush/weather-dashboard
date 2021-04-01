a41a088d697e261c5733ef42b48d1dae3e58c339
Tanya Y <brulek7@gmail.com>
2021-04-01 14:32:52 -0400

    created fetch function

--- Assets/weather.js
+++ Assets/weather.js
@@ -1,2 +1,22 @@
-// API call: api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=0958bd1155a3f8fcc1d1b82c92089c33
+//API call: http://api.openweathermap.org/data/2.5/forecast?q=toronto&appid=0958bd1155a3f8fcc1d1b82c92089c33 
+// URL: 'https://api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
+
+var apiKey = "0958bd1155a3f8fcc1d1b82c92089c33";
+var searchBtn = $(".searchBtn");
+var searchInput = $(".searchInput");
+
+var searchCity = "ottawa";
+var weatherContent;
+
+function getTopWeather() {
+    var apiURL = 'http://api.openweathermap.org/data/2.5/forecast?q=' + searchCity + '&appid=0958bd1155a3f8fcc1d1b82c92089c33';
+    fetch(apiURL)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        console.log(data.city.name)
+        console.log(data.list[0]) 
+        });
+};
+getTopWeather();
 
